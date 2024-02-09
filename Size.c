5cbbebef149a460efe7e04350707d2a404ed399c
BenGJ10 <bengj1015@gmail.com>
2024-02-09 21:10:40 +0530

    Size of int, float, double & char in bytes

---
+++ Size.c
@@ -0,0 +1,9 @@
+// Find the size of int,float,double,and char
+#include <stdio.h>
+int main(){
+    printf("Size of integer is %d byte.\n",sizeof(int));
+    printf("Size of float is %d byte.\n",sizeof(float));
+    printf("Size of double is %d byte.\n",sizeof(double));
+    printf("Size of char is %d byte.\n",sizeof(char));
+    return 0;
+}
