074e238da923b426c35710ad8b73cfc28873553d
BenGJ10 <bengj1015@gmail.com>
2024-02-09 21:40:18 +0530

    Convert fahrenheit to celcius

--- Fahrenheit.c
+++ Fahrenheit.c
@@ -1,10 +1,10 @@
 // Write a C program to convert fahrenheit to celsius
 #include <stdio.h>
 int main(){
-    float f,c;
+    float fah,cel;
     printf("Enter fahrenheit value: ");
-    scanf("%f",&f);
-    c = ((f-32)*(5/9));     // Equation to convert fahrenheit to celsius
-    printf("%.0f fahrenheit is %.0f degree celcuis\n",f,c);
+    scanf("%f",&fah);
+    cel = ((fah-32)*(5/9));     // Equation to convert fahrenheit to celsius
+    printf("%.0f fahrenheit is %.0f degree celcuis\n",fah,cel);
     return 0;
 }
