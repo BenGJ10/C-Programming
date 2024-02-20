db8bbf2b3e81f25187879351b2f0a9edf7812a94
BenGJ10 <bengj1015@gmail.com>
2024-02-20 22:49:24 +0530

    Contains various number, star & alphabet patterns

---
+++ Pattern-Printing/NumPattern3.c
@@ -0,0 +1,14 @@
+// Program to print Inverted Left Half number pyramid in C
+#include <stdio.h>
+int main(){
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+    for(int i=rows; i>=1; i--){
+        for(int j=i; j>=1; j--){
+            printf("%d ",j);
+        }
+        printf("\n");}
+    
+    return 0;
+}
